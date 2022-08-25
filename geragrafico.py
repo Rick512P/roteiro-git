093f2ebf7ee482c113e164cfc254eccc977f31b8
Érick <ricksantosmatos@gmail.com>
2022-08-25 09:55:16 -0300

    Update geragrafico.py

--- geragrafico.py
+++ geragrafico.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 from leitorarquivo import LeitorArquivo
  
 def main():
+    
     leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
     
+    plt.title('Gráfico de linhas')
+    
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
-    
-    plt.plot(valores)
+    for serie in valores:
+        plt.plot(serie)
+
     plt.show()
 
 main()
